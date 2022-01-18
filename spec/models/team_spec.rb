d5f4b428d5019131b8837d0f2669d47723c33d41
Eric Chon <echon92@gmail.com>
2022-01-18 10:04:57 -0700

    created test for average age of team

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -8,16 +8,15 @@ RSpec.describe Team, type: :model do
   it "checks for the average of all player on a team" do
 
     competition1 = Competition.create!(name: "Competition 1", location: "Location 1", sport: "Sport 1")
-    competition2 = Competition.create!(name: "Competition 2", location: "Location 1", sport: "Sport 1")
 
     team1 =  Team.create!(hometown: "hometown 1", nickname: "nickname 1")
-    team2 = Team.create!(hometown: "hometown 2", nickname: "nickname 2")
 
     ct1 = CompetitionTeam.create!(competition: competition1, team: team1)
-    ct2 = CompetitionTeam.create!(competition: competition1, team: team2)
 
-    player1 = Player.create!()
+    player1 = Player.create!(name: "name 1", age: 20, team: team1)
+    player2 = Player.create!(name: "name 2", age: 25, team: team1)
+    player3 = Player.create!(name: "name 3", age: 30, team: team1)
 
-    expect()
+    expect(team1.average_age).to eq(25)
   end
 end
